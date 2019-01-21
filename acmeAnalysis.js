6dd999a86dd7c9da8ebbfd11dc6fc7c4062e6feb
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-01-21 13:41:05 -0500

    Completed First Method Plus Test

--- acmeAnalysis.js
+++ acmeAnalysis.js
@@ -1,1 +1,78 @@
-module.exports = acmeAnalysis;
+const products = [
+  {
+    id: 1,
+    name: 'foo',
+    price: 7,
+  },
+  {
+    id: 2,
+    name: 'bar',
+    price: 2,
+  },
+  {
+    id: 5,
+    name: 'bazz',
+    price: 1,
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    name: 'moe',
+  },
+  {
+    id: 2,
+    name: 'larry',
+  },
+  {
+    id: 3,
+    name: 'curly',
+  },
+];
+const orders = [
+  {
+    id: 1,
+    productId: 1,
+    quantity: 3,
+    userId: 1,
+  },
+  {
+    id: 2,
+    productId: 1,
+    quantity: 7,
+    userId: 1,
+  },
+  {
+    id: 3,
+    productId: 5,
+    quantity: 70,
+    userId: 3,
+  },
+  {
+    id: 4,
+    productId: 5,
+    quantity: 1,
+    userId: 3,
+  },
+];
+
+const productsPurchased = (orderVals, productVals) => {
+  //reduce the products array to only the products that have been purchased
+  return productVals.reduce((accumulator, currentValue) => {
+    //search the orders for the product id
+    let found = orderVals.find(element => {
+      return element.productId === currentValue.id;
+    });
+    //console.log(`Acc right now is ${accumulator}`);
+    //return accumulator.concat(currentValue);
+    if (found) accumulator.push(currentValue);
+    return accumulator;
+  }, []);
+};
+
+console.log(productsPurchased(orders, products));
+
+//why does this give me an error when I run it in node
+//Also the test was still able to pick up the file even with this commented out
+//module.exports = acmeAnalysis;
--- acmeAnalysis.spec.js
+++ acmeAnalysis.spec.js
@@ -1,6 +1,10 @@
 const expect = require('chai').expect;
-const acmeAnalysis = './acmeAnalysis';
+//make a const for each function of struture that you will be testing inside of the file being tested
+const productsPurchased = './acmeAnalysis';
 
 describe('acmeAnalysis', () => {
-  it('exists', () => {});
+  it('exists', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(productsPurchased).to.be.ok;
+  });
 });
